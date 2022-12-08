8bc3b85e64d0d3af68ebd0d54eb803ce1ef1cdc6
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-12-08 16:40:37 -0600

    work through lessons on c#

--- TestApp2/Program.cs
+++ TestApp2/Program.cs
@@ -2,7 +2,14 @@
 {
     static void Main()
     {
-        System.Console.WriteLine("Hello World");
+        String name = "Scott";
+        int age = 23;
+
+        System.Console.Write("hey ");
+        System.Console.Write(name);
+        System.Console.Write(" your age is ");
+        System.Console.Write(age);
+
         System.Console.ReadKey();
     }
 }
