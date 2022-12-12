aec13ea9a90911b04d5df446ed537173156dd76e
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-12-12 13:34:13 -0600

    comment out old code and follow along and complete assignments

--- TestApp2/Program.cs
+++ TestApp2/Program.cs
@@ -2,14 +2,62 @@
 {
     static void Main()
     {
-        String name = "Scott";
-        int age = 23;
+        //    String name = "Scott";
+        //    int age = 23;
 
-        System.Console.Write("hey ");
-        System.Console.Write(name);
-        System.Console.Write(" your age is ");
-        System.Console.Write(age);
+        //    System.Console.Write("hey ");
+        //    System.Console.Write(name);
+        //    System.Console.Write(" your age is ");
+        //    System.Console.Write(age);
 
+        //    System.Console.ReadKey();
+
+        //area of a circle assignment
+        //int radius = 3;
+
+        //System.Console.Write(3.14 * radius * radius);
+
+        //height in inches to centimeters
+        //double feet = 6;
+        //double inches = 2;
+        //double oneInch = 2.54;
+        //double oneFoot = 12;
+
+        //double feetInInches = (feet * oneFoot) + inches;
+
+        //System.Console.Write(feetInInches * oneInch);
+
+        //how to round to the nearest thousandth without using commands
+        //int input = 417;
+        //decimal decimalInput = input;
+
+        //int prevThousand = (input / 1000) * 1000;
+
+        //decimal lastThree1 = decimalInput / 1000;
+        //decimal lastThree2 = input /1000;
+        //decimal getLastThree = (lastThree1 - lastThree2) * 1000;
+
+        //int round = (getLastThree >= 500) ? 1000 : 0;
+        //int lessThanfiveHundred = (input < 500) ? 1000 : 0;
+
+        //int nearestThousand = prevThousand + round + lessThanfiveHundred;
+
+        //System.Console.WriteLine(nearestThousand);
+
+        int seconds = 288970;
+        int minutes = seconds / 60;
+        int hours = seconds / 60 / 60;
+        int days = seconds / 60 / 60 / 24;
+
+        int secondsLeft = seconds - (minutes * 60);
+        int minutesLeft = minutes - (hours * 60);
+        int hoursLeft = hours - (days * 24);
+
+
+        System.Console.WriteLine(days + " days, " + hoursLeft + " hours, " + minutesLeft + " minutes, " + secondsLeft + " seconds");
+   
         System.Console.ReadKey();
+
     }
+
 }
---
+++ TestApp2/TypeClass.cs
@@ -0,0 +1,26 @@
+﻿class NewSample
+{
+    //static void Main()
+    //{
+    //    sbyte a = sbyte.MaxValue;
+    //    sbyte b = 100;
+    //    sbyte c = default(sbyte);
+
+    //    byte d = byte.MaxValue;
+    //    byte e = default(byte);
+
+    //    char f = 'A';
+    //    string g = "hello";
+
+    //    System.Console.WriteLine(a);
+    //    System.Console.WriteLine(b);
+    //    System.Console.WriteLine(c);
+    //    System.Console.WriteLine(d);
+    //    System.Console.WriteLine(e);
+    //    System.Console.WriteLine(f);
+    //    System.Console.WriteLine(g);
+
+    //    System.Console.ReadKey();
+    //}
+}
+
